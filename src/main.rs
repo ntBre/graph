dc69c68d4d1198a331d4661c88259c5a7f711310
Brent Westbrook <brentrwestbrook@gmail.com>
2022-11-02 11:56:14 -0400

    use simplified api

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use std::fs::File;
-use std::io::BufWriter;
-use image::{Encoder, Image, Rgba};
+use image::{Image, Rgba};
 
 fn main() {
-    let w = BufWriter::new(File::create("try.png").unwrap());
-
     const HEIGHT: u32 = 100;
     const WIDTH: u32 = 100;
-
-    let encoder = Encoder::new(w, WIDTH, HEIGHT);
-    let writer = encoder.write_header().unwrap();
 
     let mut img = Image::new(WIDTH, HEIGHT);
     img.fill(Rgba::black());
@@ -17,5 +10,5 @@ fn main() {
     img.set(50, 50, Rgba::green());
     img.set(50, 90, Rgba::blue());
 
-    img.write(writer).unwrap();
+    img.write("try.png").unwrap();
 }
